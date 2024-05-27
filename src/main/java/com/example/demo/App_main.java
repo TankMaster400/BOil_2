64f4eb9cdedd73c961c50ff9beeeb15e972958e9
Franciszek Stolarczyk <franciszek.stolarczyk@gmail.com>
2024-05-27 10:20:31 +0200

    Wpisywanie danych

--- src/main/java/com/example/demo/App_main.java
+++ src/main/java/com/example/demo/App_main.java
@@ -1,42 +1,26 @@
 package com.example.demo;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class App_main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(App_main.class.getResource("View_1.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        stage.setTitle("App");
         stage.setScene(scene);
         stage.show();
-
-        int[][] grid = new int[][] {
-                { 8, 14, 17}, { 12, 9, 19 }
-        }; // table
-
-        int[] supply
-                = new int[] { 20,30 }; // supply
-        int[] demand
-                = new int[] { 10, 28 ,27};
-        int[] zakup
-                = new int[] { 10, 12 }; // supply
-        int[] sprzedaz
-                = new int[] { 30, 25, 30 };
-
-        Obliczenia ob1 = new Obliczenia(grid, supply,demand,zakup, sprzedaz);
-        ob1.oblicz_j();
-        ob1.oblicz_pop();
-        ob1.oblicz_zar();
-        ob1.oblicz_k();
-        ob1.oblicz_z();
-
-
     }
 
     public static void main(String[] args) throws IOException {
